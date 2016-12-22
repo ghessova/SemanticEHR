f747ec1df12af79c25486c0468a13260d8407fe3
David Herman <herman.david@protonmail.com>
2016-12-22 18:59:30 +0100

    AuthenticationResource swagger documentation

---
+++ src/main/java/cz/zcu/kiv/sehr/model/AuthenticationResponse.java
@@ -0,0 +1,29 @@
+package cz.zcu.kiv.sehr.model;
+
+
+/**
+ * Response used by AuthenticationResource on login
+ *
+ */
+public class AuthenticationResponse {
+
+    String accessToken;
+
+    UserWrapper user;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public UserWrapper getUser() {
+        return user;
+    }
+
+    public void setUser(UserWrapper user) {
+        this.user = user;
+    }
+}
--- src/main/java/cz/zcu/kiv/sehr/resources/AuthenticationResource.java
+++ src/main/java/cz/zcu/kiv/sehr/resources/AuthenticationResource.java
@@ -1,18 +1,22 @@
 package cz.zcu.kiv.sehr.resources;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.bson.Document;
+
+import cz.zcu.kiv.sehr.model.AuthenticationResponse;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * Resource maintaing user authentication
@@ -29,8 +33,8 @@ public class AuthenticationResource {
      */
     @GET
     @Path("login")
-    @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value="Check if user is logged in", response = Document.class)
+    @ApiOperation(value="Check if user is logged in")
+    @ApiResponses(value = { @ApiResponse(code = 204, message = "Token is valid"), @ApiResponse(code = 403, message = "Invalid access token") } )
     public Response isLogged(@HeaderParam("token") String apiToken) {
 
         // TODO Write logic of checking token validity
@@ -47,7 +51,8 @@ public class AuthenticationResource {
     @POST
     @Path("login")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value="Authenticate user", response = Document.class)
+    @ApiOperation(value="Authenticate user", response = AuthenticationResponse.class)
+    @ApiResponses(value = { @ApiResponse(code = 403, message = "Username or password") } )
     public Response loginUser(@FormParam("user") String user, @FormParam("password") String password) {
 
         // TODO Write logic of actuall user creating
@@ -64,6 +69,7 @@ public class AuthenticationResource {
     @POST
     @Path("logout")
     @ApiOperation(value="Invalidate user's token")
+    @ApiResponses(value = { @ApiResponse(code = 204, message = "Token is valid"), @ApiResponse(code = 403, message = "Invalid access token") } )
     public Response destroyToken(@HeaderParam("token") String apiToken) {
 
         // TODO Write logic of destroying token
